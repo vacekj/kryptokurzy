4b20f6e111b74457ae6d27c9a5139e44fc184b5b
Josef Vacek <vacekj@outlook.com>
2020-12-27 12:20:16 +0100

    fix: meta description

--- util/DefaultSEO.ts
+++ util/DefaultSEO.ts
@@ -1,4 +1,6 @@
 export default {
+	description:
+		"Jaknacrypto.cz je první blockchainová a crypto vzdělávací platforma v češtině. Poskytujeme nejlepší informace bezplatně a máme strukturované kurzy které Vás provedou tím jak tyhle technologie fungují, jak do nich bezpečně investovat, jak s kryptoměnami obchodovat na směnárnách a jak programovat na blockchainu.",
 	openGraph: {
 		type: "website",
 		locale: "cs_CS",
