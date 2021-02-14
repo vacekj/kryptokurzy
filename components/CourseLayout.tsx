bd15cd95fd6e641f2c123ffb3cf87e8ff393d97b
Dominik <dominik.clemente@gmail.com>
2021-02-14 21:05:03 +0100

    add(basic structure)

--- components/CourseLayout.tsx
+++ components/CourseLayout.tsx
@@ -1,11 +1,14 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { Box } from "@chakra-ui/react";
 
 export default function CourseLayout(props: { children: JSX.Element }) {
 	return (
 		<>
 			<Navbar />
-			{props.children}
+			<Box m={10} pl={5} pb={10}>
+				{props.children}
+			</Box>
 			<Footer />
 		</>
 	);
